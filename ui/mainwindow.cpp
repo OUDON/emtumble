83d88f337d42adffd458362d9b95b946a0f9d3b0
T.Tomita <ttp8101@gmail.com>
2017-11-30 11:52:32 +0900

    Implemented play button

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -10,6 +10,9 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    timer_simulation_delay = new QTimer(this);
+    connect(timer_simulation_delay, SIGNAL(timeout()), this, SLOT(step_board()));
+
     scene = new QGraphicsScene(this);
     ui->graphics_view->setScene(scene);
     GUI::get_instance().draw(scene);
@@ -20,6 +23,12 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::step_board()
+{
+    GUI::get_instance().step();
+    update_graphics();
+}
+
 void MainWindow::update_graphics()
 {
     GUI::get_instance().draw(scene);
@@ -27,8 +36,7 @@ void MainWindow::update_graphics()
 
 void MainWindow::on_button_step_clicked()
 {
-    GUI::get_instance().step();
-    update_graphics();
+    step_board();
 }
 
 void MainWindow::on_action_open_triggered()
@@ -36,4 +44,15 @@ void MainWindow::on_action_open_triggered()
     QString file_name = QFileDialog::getOpenFileName(this,"select file");
     GUI::get_instance().load_board(file_name.toStdString());
     update_graphics();
+}
+
+void MainWindow::on_button_play_clicked()
+{
+    if (timer_simulation_delay->isActive()) {
+        timer_simulation_delay->stop();
+        ui->button_play->setText("Play");
+    } else {
+        timer_simulation_delay->start(500);
+        ui->button_play->setText("Stop");
+    }
 }
--- ui/mainwindow.h
+++ ui/mainwindow.h
@@ -19,10 +19,14 @@ public:
 private slots:
     void on_button_step_clicked();
     void on_action_open_triggered();
+    void on_button_play_clicked();
+
+    void step_board();
 
 private:
     Ui::MainWindow *ui;
     QGraphicsScene *scene;
+    QTimer *timer_simulation_delay;
 
     void update_graphics();
 };
